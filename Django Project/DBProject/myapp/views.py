3f76bad9cbabeef0f81c34a205fa5c5f413bf544
SanketTops <sanket.tops@gmail.com>
2024-04-13 08:33:28 +0530

    Full CRUD

--- Django Project/DBProject/myapp/views.py
+++ Django Project/DBProject/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import *
 
 # Create your views here.
@@ -15,3 +15,21 @@ def index(request):
 def showdata(request):
     data=userinfo.objects.all()
     return render(request,'showdata.html',{'data':data})
+
+
+def deletedata(request,id):
+    userid=userinfo.objects.get(id=id)
+    userinfo.delete(userid)
+    return redirect('showdata')
+
+def updatedata(request,id):
+    userid=userinfo.objects.get(id=id)
+    if request.method=='POST':
+        update=userForm(request.POST,instance=userid)
+        if update.is_valid():
+            update.save()
+            print("Your data has been updated!")
+            return redirect('showdata')
+        else:
+            print(update.errors)
+    return render(request,'updatedata.html',{'userid':userid})
