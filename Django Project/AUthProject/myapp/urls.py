ef11d784aefdda52978089eb83383b6f1ca5731f
SanketTops <sanket.tops@gmail.com>
2024-04-16 08:59:15 +0530

    login-signup

--- Django Project/AUthProject/myapp/urls.py
+++ Django Project/AUthProject/myapp/urls.py
@@ -4,4 +4,6 @@ from myapp import views
 
 urlpatterns = [
     path('',views.index),
+    path('signup/',views.signup),
+    path('home/',views.home,name='home'),
 ]
--- Django Project/AUthProject/myapp/views.py
+++ Django Project/AUthProject/myapp/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import *
 
 # Create your views here.
 def index(request):
+    if request.method=='POST':
+
+        unm=request.POST['username']
+        pas=request.POST['password']
+
+        user=usersignup.objects.filter(username=unm,password=pas)
+        if user: 
+            print("Login Successfull!")
+            return redirect('home')
+        else:
+            print("Error!Login Faild....")
     return render(request,'index.html')
+
+def signup(request):
+    if request.method=='POST':
+        newuser=signupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+            return redirect('/')
+        else:
+            print(newuser.errors)
+    return render(request,'signup.html')
+
+def home(request):
+    return render(request,'home.html')
